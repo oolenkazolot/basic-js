7763b0476719d9696866049675cb7c8e659257f9
oolenkazolot <oolenka.zolot@gmail.com>
2022-10-14 21:25:51 +0200

    feat: resolved 15 tasks

--- test/sort-by-height.test.js
+++ test/sort-by-height.test.js
@@ -8,25 +8,10 @@ Object.freeze(assert);
 
 describe('Sort by height', () => {
   it.optional('should return sorted array without moving -1', () => {
-    assert.deepEqual(
-      sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]),
-      [-1, 150, 160, 170, -1, -1, 180, 190],
-    );
-    assert.deepEqual(
-      sortByHeight([-1, -1, -1, -1, -1]),
-      [-1, -1, -1, -1, -1],
-    );
-    assert.deepEqual(
-      sortByHeight([-1]),
-      [-1],
-    );
-    assert.deepEqual(
-      sortByHeight([4, 2, 9, 11, 2, 16]),
-      [2, 2, 4, 9, 11, 16],
-    );
-    assert.deepEqual(
-      sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]),
-      [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77],
-    );
+    assert.deepEqual(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]), [-1, 150, 160, 170, -1, -1, 180, 190]);
+    assert.deepEqual(sortByHeight([-1, -1, -1, -1, -1]), [-1, -1, -1, -1, -1]);
+    assert.deepEqual(sortByHeight([-1]), [-1]);
+    assert.deepEqual(sortByHeight([4, 2, 9, 11, 2, 16]), [2, 2, 4, 9, 11, 16]);
+    assert.deepEqual(sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]), [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77]);
   });
 });
