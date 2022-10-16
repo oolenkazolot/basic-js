711ac7e000eaa825fbf0120e9b48e1f750bc914e
oolenkazolot <oolenka.zolot@gmail.com>
2022-10-16 19:44:06 +0200

    resolved everything

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,30 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  sampleActivity = +sampleActivity;
+  if (!sampleActivity || sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const res = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  return Math.ceil(res);
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,84 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(revers) {
+    this.revers = revers === false ? true : false;
+    this.en = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let res = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+      const charIndex = this.en.indexOf(char);
+      if (charIndex === -1) {
+        res.push(char);
+        continue;
+      }
+      const k = Math.floor(j / key.length);
+      const charKey = key[j - k * key.length];
+      const keyIndex = this.en.indexOf(charKey);
+      const kR = Math.floor((charIndex + keyIndex) / this.en.length);
+      const newChar = this.en[charIndex + keyIndex - kR * this.en.length];
+      j++;
+      res.push(newChar);
+    }
+
+    return this.revers ? res.reverse().join('') : res.join('');
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let res = [];
+    const message = encryptedMessage.toUpperCase();
+
+    key = key.toUpperCase();
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+      const charIndex = this.en.indexOf(char);
+      if (charIndex === -1) {
+        res.push(char);
+        continue;
+      }
+      const k = Math.floor(j / key.length);
+      const charKey = key[j - k * key.length];
+      const keyIndex = this.en.indexOf(charKey);
+      const kR = Math.floor((charIndex - keyIndex) / this.en.length);
+      const newChar = this.en[charIndex - keyIndex - kR * this.en.length];
+      j++;
+      res.push(newChar);
+    }
+
+    return this.revers ? res.reverse().join('') : res.join('');
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
